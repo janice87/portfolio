bdf6d9f716fcf8125e06d325a78b5336dced6a1e
Janice Chan <janice.chan1288@gmail.com>
2022-10-24 14:06:17 -0400

    add about section

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// add pic
+const About = () => {
+  return (
+    <div className="about">
+       <h1>ABOUT ME</h1>
+        <h2>Janice Chan</h2>
+        <p>Based in New York City</p>
+        <p>Flatiron School Software Engineering Grad</p>
+        <p>Masters in Accounting</p>
+        <p>12+ Years of Experience in Finance/Accounting</p>
+        <p>Skills: </p>
+    </div>
+  )
+}
+
+export default About
