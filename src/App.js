4aaed0577e3466efc9141f4cf3c664d798eb937c
Janice Chan <janice.chan1288@gmail.com>
2022-10-24 14:05:31 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import NavBar from './components/NavBar'
+import Banner from './components/Banner'
+import About from './components/About'
+import Projects from './components/Projects';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <About />
+      <Projects />
     </div>
   );
 }
