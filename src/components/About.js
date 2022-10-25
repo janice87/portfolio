69d915aabb9798ee5817843a9b26ef301319368a
Janice Chan <janice.chan1288@gmail.com>
2022-10-25 18:45:11 -0400

    add about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import pic from '../assets/img/pic.png'
+import about from '../assets/img/about.jpg'
+//import aboutSection from '../assets/img/about-section.jpg'
 
-// add pic
+
 const About = () => {
   return (
     <div className="about">
-       <h1>ABOUT ME</h1>
-        <h2>Janice Chan</h2>
-        <p>Based in New York City</p>
-        <p>Flatiron School Software Engineering Grad</p>
-        <p>Masters in Accounting</p>
-        <p>12+ Years of Experience in Finance/Accounting</p>
+       <h1>ABOUT</h1>
+       <hr style={{border: "3px solid #dfcac1"}}/>
+       <img src={pic} alt="my_picture" className="about-pic" />
+       <img src={about} alt="about_section" className="about-image" />
+       {/* <img src={about} alt="about_section" className="about-image" /> */}
+        {/* <h3>Janice Chan</h3>
+        <ul>
+          <li>Born & Raised in New York City</li>
+          <li>Grad of Flatiron School's Software Engineering program</li>
+          <li>I have a Masters in Accounting</li>
+          <li>Background in Finance/Accounting with 12+ years of experience</li>
+          <li>I am a team player, problem solver and am highly analytical.</li>
+        </ul>       */}
         <p>Skills: </p>
     </div>
   )
